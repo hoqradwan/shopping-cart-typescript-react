8e6da81a9f6227ee50cf5db4989ccea991ce5f76
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-10-04 07:47:01 +0600

    essential functions added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap"
 import { Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
@@ -9,14 +10,17 @@ function App() {
 
   return (
     <>
-      <Navbar></Navbar>
-      <Container className="mb-4">
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/store" element={<Store />}></Route>
-          <Route path="/about" element={<About />}></Route>
-        </Routes>
-      </Container>
+      <ShoppingCartProvider>
+        <Navbar></Navbar>
+        <Container className="mb-4">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/store" element={<Store />}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
+        </Container>
+      </ShoppingCartProvider>
+
     </>
 
   )
