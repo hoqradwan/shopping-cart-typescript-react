aeb3a82dd921ef353cc1be772c2dc1aecaabe578
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-10-04 10:07:18 +0600

    project completed

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Card, Button } from 'react-bootstrap';
+import { useShoppingCart } from '../context/ShoppingCartContext';
 import { formatCurrency } from '../utilities/formatCurrency';
 type StoreItemProps = {
     id: number;
@@ -7,7 +8,8 @@ type StoreItemProps = {
     imgUrl: string;
 }
 const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
-    const quantity: number = 0;
+    const {getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart} = useShoppingCart()
+    const quantity = getItemQuantity(id);
     return (
         <>
             <Card className='h-100'>
@@ -23,13 +25,13 @@ const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
                         <span className='ms-2 text-muted'>{formatCurrency(price)}</span>
                     </Card.Title>
                     <div className='mt-auto'>
-                        {quantity === 0 ? <Button className='w-100'>Add to cart</Button> : (<div
+                        {quantity === 0 ? <Button className='w-100' onClick={()=>increaseCartQuantity(id)}>Add to cart</Button> : (<div
                             className="d-flex align-items-center flex-column"
                             style={{ gap: ".5rem" }}
                         >      <div
                             className="d-flex align-items-center justify-content-center"
                             style={{ gap: ".5rem" }}
-                        ><Button>-</Button> <div><span className='fs-3'>{quantity}</span></div> <Button>+</Button></div><Button variant='danger' size='sm'>Remove</Button></div>)}
+                        ><Button onClick={()=>decreaseCartQuantity(id)}>-</Button> <div><span className='fs-3'>{quantity}</span></div> <Button onClick={()=>increaseCartQuantity(id)}>+</Button></div><Button variant='danger' size='sm' onClick={()=>removeFromCart(id)}>Remove</Button></div>)}
                     </div>
                 </Card.Body>
             </Card>
